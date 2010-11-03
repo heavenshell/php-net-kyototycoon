6eb083dcca40cebe812d7f2a5186a79c9addebf7
s_ohyanagi <heavenshell.jp@gmail.com>
2010-11-04 01:18:14 +0900

    Add port not to use default

--- examples/simple.php
+++ examples/simple.php
@@ -1,13 +1,12 @@
 <?php
+// Start KyotoTycoon port 19780
+// $ ktserver -port 19780
 error_reporting(E_ALL | E_STRICT);
 $src = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src';
-set_include_path(get_include_path()
-    . $src
-);
+set_include_path(get_include_path() . $src);
 
 require_once 'Net/KyotoTycoon.php';
-$kt = new \Net\KyotoTycoon();
+$kt = new \Net\KyotoTycoon(array('port' => 19780));
 $kt->set('test_php', 'Hello KyotoTycoon!!');
 var_dump($kt->get('test_php'));
-
 $kt->clear();
