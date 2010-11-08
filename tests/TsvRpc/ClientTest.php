65aa07c6f5bb5485d0af09a46473f61c7f8de81e
s_ohyanagi <heavenshell.jp@gmail.com>
2010-11-08 22:11:32 +0900

    Fix require_once statement.

--- tests/TsvRpc/ClientTest.php
+++ tests/TsvRpc/ClientTest.php
@@ -69,7 +69,7 @@ class ClientTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        require_once('HTTP/TsvRpc/Client.php');
+        require_once 'HTTP/TsvRpc/Client.php';
     }
 
     /**
